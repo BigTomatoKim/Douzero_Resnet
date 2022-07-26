2081091d54019ad414d26acfcf9cabf1ab57290e
BigTomatoKim <741852073@qq.com>
2022-07-26 14:14:03 +0800

    fix

--- BidModel.py
+++ BidModel.py
@@ -9,21 +9,22 @@ import time
 
 
 def EnvToOnehot(cards):
-    Env2IdxMap = {3:0,4:1,5:2,6:3,7:4,8:5,9:6,10:7,11:8,12:9,13:10,14:11,17:12,20:13,30:14}
+    Env2IdxMap = {3: 0, 4: 1, 5: 2, 6: 3, 7: 4, 8: 5, 9: 6, 10: 7, 11: 8, 12: 9, 13: 10, 14: 11, 17: 12, 20: 13, 30: 14}
     cards = [Env2IdxMap[i] for i in cards]
-    Onehot = torch.zeros((4,15))
+    Onehot = torch.zeros((4, 15))
     for i in range(0, 15):
-        Onehot[:cards.count(i),i] = 1
+        Onehot[:cards.count(i), i] = 1
     return Onehot
+
 
 def RealToOnehot(cards):
     RealCard2EnvCard = {'3': 0, '4': 1, '5': 2, '6': 3, '7': 4,
                         '8': 5, '9': 6, 'T': 7, 'J': 8, 'Q': 9,
                         'K': 10, 'A': 11, '2': 12, 'X': 13, 'D': 14}
     cards = [RealCard2EnvCard[c] for c in cards]
-    Onehot = torch.zeros((4,15))
+    Onehot = torch.zeros((4, 15))
     for i in range(0, 15):
-        Onehot[:cards.count(i),i] = 1
+        Onehot[:cards.count(i), i] = 1
     return Onehot
 
 
@@ -63,6 +64,7 @@ if os.path.exists("./bid_weights.pkl"):
     else:
         net.load_state_dict(torch.load('./bid_weights.pkl', map_location=torch.device("cpu")))
 
+
 def predict(cards):
     input = RealToOnehot(cards)
     if UseGPU:
@@ -70,6 +72,7 @@ def predict(cards):
     input = torch.flatten(input)
     win_rate = net(input)
     return win_rate[0].item() * 100
+
 
 def predict_env(cards):
     input = EnvToOnehot(cards)
